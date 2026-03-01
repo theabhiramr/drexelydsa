c216ab8e0fa0e614f98c86f05c249577bce43865
theabhiramr <ramachandran.abhiram@gmail.com>
2026-02-28 20:11:59 -0500

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-screen bg-background-secondary -z-10" />
